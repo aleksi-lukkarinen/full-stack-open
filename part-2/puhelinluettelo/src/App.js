ae13211674931e82a423b1ae008b46d4b6df9c1a
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-14 00:40:12 +0300

    Ex 2.7 done

--- part-2/puhelinluettelo/src/App.js
+++ part-2/puhelinluettelo/src/App.js
@@ -2,19 +2,46 @@ import React, { useState } from 'react'
 import NumberListLine from './components/NumberListLine'
 
 const App = () => {
+  const config = {
+    errAddingEmptyName: "An empty name cannot be added to the phonebook.",
+    errAddingDuplicateEntry: "A person with name {} has already been added to the phonebook; another one cannot be added."
+  }
   const [entries, setEntries] = useState([
     { name: 'Arto Hellas' }
   ])
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState("")
 
   const addEntry = (event) => {
     event.preventDefault()
 
-    const entryToAdd = {
-      name: newName,
+    let errMsg = undefined;
+    const trimmedName = newName.trim()
+
+    if (trimmedName.length < 1) {
+      errMsg = config.errAddingEmptyName
+    }
+    else {
+      const ucaseName = trimmedName.toUpperCase()
+      const testIfNameExists = e => e.name.toUpperCase() === ucaseName
+      const existingEntry = entries.find(testIfNameExists)
+
+      if (existingEntry) {
+        errMsg = config.errAddingDuplicateEntry.replace(
+                    "{}", existingEntry.name)
+      }
     }
 
-    setEntries(entries.concat(entryToAdd))
+    if (errMsg) {
+      alert(errMsg)
+    }
+    else {
+      const entryToAdd = {
+        name: trimmedName,
+      }
+
+      setEntries(entries.concat(entryToAdd))
+    }
+
     setNewName("")
   }
 
@@ -27,10 +54,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addEntry}>
         <div>
-          name: <input value={newName} onChange={updateName} />
+          Name: <input value={newName} onChange={updateName} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
