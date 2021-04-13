95e84ff4563bcef94e3cf0994dbf23b51ae263ff
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-13 15:17:16 +0300

    Exs 2.3, 2.4, and 2.5 done

--- part-2/kurssitiedot/src/components/CourseTotalParts.js
+++ part-2/kurssitiedot/src/components/CourseTotalParts.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
-const CourseTotalParts = (x) => {
-  console.log(x)
-  let {parts} = x
-  console.log(parts)
+const CourseTotalParts = ({parts}) => {
   const totalExercises =
       parts.map(x=>x.exercises).reduce((x, y) => x + y, 0)
 
