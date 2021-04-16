a151efdb766bc197f5d74d500b891d47b66a6b55
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-16 16:42:42 +0300

    Ex. 2.14 done

--- part-2/maiden-tiedot/src/Config.js
+++ part-2/maiden-tiedot/src/Config.js
@@ -1,15 +1,29 @@
-export const APPLICATION_TITLE="Country Information"
+export const APPLICATION_TITLE = "Country Information"
 
-export const MSG_RETRIEVING_COUNTRY_DATA="Retrieving country data..."
-export const MSG_TOO_MANY_MATCHES="Too many matches. Please adjust the criterion."
+export const MSG_RETRIEVING_COUNTRY_DATA = "Retrieving country data..."
+export const MSG_TOO_MANY_MATCHES = "Too many matches. Please adjust the criterion."
 
-export const FIELD_LABEL_SEARCH="Search for countries"
-export const BUTTON_LABEL_SHOW="Show"
+export const ERR_MSG_WEATHER_SRVC_CNN_ERR =
+          "An error occurred while contacting weather service"
 
-export const LABEL_CAPITAL="Capital"
-export const LABEL_POPULATION="Population"
-export const LABEL_LANGUAGES="Languages"
-export const LABEL_FLAG="Flag"
-export const KEY_VALUE_SEPARATOR=":"
+export const ERR_MSG_WEATHER_SRVC_API_KEY_MISSING =
+          "Weather service API key has not been set!"
 
-export const URL_REST_COUNTRIES_API_ALL="https://restcountries.eu/rest/v2/all"
+export const FIELD_LABEL_SEARCH = "Search for countries"
+export const BUTTON_LABEL_SHOW = "Show"
+
+export const LABEL_CAPITAL = "Capital"
+export const LABEL_POPULATION = "Population"
+export const LABEL_LANGUAGES = "Languages"
+export const LABEL_FLAG = "Flag"
+export const LABEL_CURRENT_WEATHER_AT = "Current weather at"
+export const LABEL_TEMPERATURE = "Temperature"
+export const LABEL_WIND = "Wind"
+export const KEY_VALUE_SEPARATOR = ":"
+
+export const URL_REST_COUNTRIES_API_ALL = "https://restcountries.eu/rest/v2/all"
+
+export const WEATHERSTACK_APIKEY = process.env.REACT_APP_COUNTRY_INFO_WEATHERSTACK_API_KEY
+export const WEATHERSTACK_BASE_URL = "http://api.weatherstack.com/"
+export const WEATHERSTACK_CURRENT_QUERY_URL =
+    WEATHERSTACK_BASE_URL + "current?access_key={}&query="
--- part-2/maiden-tiedot/src/components/CountryInfo.js
+++ part-2/maiden-tiedot/src/components/CountryInfo.js
@@ -1,6 +1,73 @@
 import * as conf from "../Config"
+import {useState, useEffect} from "react"
+import axios from "axios"
+
+function buildWeatherDataURL(forCountryName) {
+  let u = conf.WEATHERSTACK_CURRENT_QUERY_URL
+  u = u.replace("{}", conf.WEATHERSTACK_APIKEY)
+  u += encodeURIComponent(forCountryName)
+
+  return u
+}
+
+function isErroneousWeatherDataApiKey() {
+  return typeof(conf.WEATHERSTACK_APIKEY) !== "string" ||
+            conf.WEATHERSTACK_APIKEY.length < 1
+}
+
+function isErroneousWeatherData(data) {
+  return typeof(data) !== "object" ||
+            data === null ||
+            data.success ||
+            data.success === false
+}
+
+function weatherDataErrorOccurred(data) {
+  let errorFound = isErroneousWeatherData(data)
+
+  if (errorFound) {
+    let msg = conf.ERR_MSG_WEATHER_SRVC_CNN_ERR
+    if (data.error) {
+    if (data.error.code)
+      msg += `: ${data.error.code}`
+    if (data.error.info)
+      msg += `: ${data.error.info}`
+    }
+
+    console.error(msg)
+  }
+
+  return errorFound
+}
 
 const CountryInfo = ({country}) => {
+  const [weatherData, setWeatherData] = useState({})
+  const weatherUrl = buildWeatherDataURL(country.name)
+
+  useEffect(() => {
+    let isMounted = true
+
+    if (isErroneousWeatherDataApiKey()) {
+      console.error(conf.ERR_MSG_WEATHER_SRVC_API_KEY_MISSING)
+      return
+    }
+
+    axios
+      .get(weatherUrl)
+      .then(response => {
+        const data = response.data
+        if (isMounted && !weatherDataErrorOccurred(data)) {
+          setWeatherData(data)
+        }
+    })
+
+    return () => isMounted = false
+  }, [weatherUrl])
+
+
+  const curW = weatherData.current
+  const locW = weatherData.location
+
   return (
     <div>
       <h2>{country.name}</h2>
@@ -22,6 +89,21 @@ const CountryInfo = ({country}) => {
               alt={"The flag of " + country.name}
               style={{width: "10em", border: "1px solid gray"}} />
       </div>
+      {curW &&
+        <div>
+          <div>{conf.LABEL_CURRENT_WEATHER_AT} {locW.name} ({locW.country}){conf.KEY_VALUE_SEPARATOR}</div>
+          <div>
+            {curW.weather_descriptions.map(d => <span key={d}>{d}</span>)}
+          </div>
+          <div>{conf.LABEL_TEMPERATURE}{conf.KEY_VALUE_SEPARATOR} {curW.temperature} ℃</div>
+          <div>{conf.LABEL_WIND}{conf.KEY_VALUE_SEPARATOR} {curW.wind_dir} {curW.wind_speed} mph</div>
+          <div>
+            {curW.weather_icons.map(url =>
+              <img key={url} src={url} alt="" />
+            )}
+          </div>
+        </div>
+      }
     </div>
   )
 }
