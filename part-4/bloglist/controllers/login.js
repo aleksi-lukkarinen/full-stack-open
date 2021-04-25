30198d17c7b102af96451a2bd0158dcdb2aadea9
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-26 00:43:09 +0300

    Ex. 4.18 done

---
+++ part-4/bloglist/controllers/login.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const config = require("../utils/config")
+const loginRouter = require("express").Router()
+const User = require("../models/user")
+
+
+loginRouter.post("/", async (request, response) => {
+  const { body } = request
+
+  const user = await User.findOne({ username: body.username })
+  const correctPasswordGiven = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.passwordHash)
+
+  if (!(user && correctPasswordGiven)) {
+    return response.status(config.HTTP_STATUS_UNAUTHORIZED)
+      .set("WWW-Authenticate", "Bearer")
+      .json({ error: "invalid username or password" })
+  }
+
+  const userToReceiveToken = {
+    id: user._id,
+    username: user.username,
+  }
+
+  const token = jwt.sign(userToReceiveToken, config.SECRET_KEY)
+
+  response
+    .status(config.HTTP_STATUS_OK)
+    .send({ username: user.username, name: user.name, token })
+})
+
+module.exports = loginRouter
