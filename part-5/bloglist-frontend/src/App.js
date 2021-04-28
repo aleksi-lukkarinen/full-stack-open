1a676a99e0aeddbd0db62a5b1487ca73eea40e22
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-29 01:39:34 +0300

    Ex. 5.1 done

--- part-5/bloglist-frontend/src/App.js
+++ part-5/bloglist-frontend/src/App.js
@@ -1,15 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react"
+import loginService from "./services/login"
+import blogService from "./services/blogs"
+import Notification from "./components/Notification"
+import Blog from "./components/Blog"
+
 
 const App = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
+
+  const processLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    }
+    catch (error) {
+      setErrorMessage("Invalid credentials.")
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to BlogList</h2>
+        <form onSubmit={processLogin}>
+
+          <Notification
+            content={errorMessage}
+            baseClass={"notificationBox"}
+            messageVisibleClass={"errorVisible"} />
+
+          <div>
+            <span>Username</span>
+              <input type="text" value={username} name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            <span>Password</span>
+              <input type="password" value={password} name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -20,5 +75,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App
--- part-5/bloglist-frontend/src/components/Blog.js
+++ part-5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from "react"
+
+
 const Blog = ({blog}) => (
   <div>
     {blog.title} {blog.author}
-  </div>  
+  </div>
 )
+
 
 export default Blog
