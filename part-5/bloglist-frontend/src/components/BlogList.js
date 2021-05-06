4ef9282521d28148c58ad61e3040645aa1dccb08
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-05-06 16:21:00 +0300

    Ex. 5.9 done

--- part-5/bloglist-frontend/src/components/BlogList.js
+++ part-5/bloglist-frontend/src/components/BlogList.js
@@ -5,12 +5,34 @@ import PropTypes from "prop-types"
 import BlogListItem from "./BlogListItem"
 
 
+function compareBlogs(a, b) {
+  if (a.likes > b.likes) return -1
+  if (a.likes < b.likes) return 1
+
+  const titleA = a.title.toLowerCase()
+  const titleB = b.title.toLowerCase()
+  if (titleA < titleB) return -1
+  if (titleA > titleB) return 1
+
+  const authorA = a.author.toLowerCase()
+  const authorB = b.author.toLowerCase()
+  if (authorA < authorB) return -1
+  if (authorA > authorB) return 1
+
+  return 0
+}
+
 const BlogList = ({ blogs }) => {
-  return (
-    <>
-      {blogs.map(blog => <BlogListItem key={ blog.id } blog={ blog } />)}
-    </>
-  )
+  blogs.sort(compareBlogs)
+
+  const listElements = []
+  for (let i=0; i<blogs.length; i++) {
+    const b = blogs[Number(i)]
+    const el = <BlogListItem key={ b.id } blog={ b } />
+    listElements.push(el)
+  }
+
+  return <>{listElements}</>
 }
 
 BlogList.propTypes = {
